dcf3fa470ca509172ed8fb522f12b7780f083ad4
gojosatoru112024 <gojosatoru112024@gmail.com>
2025-01-13 13:08:24 +0000

    dataType done

--- 01_basic/02_dataTypes.js
+++ 01_basic/02_dataTypes.js
@@ -1,3 +1,25 @@
 "use strict";  // treat all js code  as a newer verson of javascript
 
-alert( 3 + 3 ); // not working beacause we are using
+// alert( 3 + 3 ); // not working beacause we are using
+
+console.log( 3 + 3);
+console.log("Gojo");
+
+let name = "GOJO"
+let age  = 22
+// let isLoggrdIn = true
+let state;
+
+// number  => 2 to power 53
+// Bigint
+// String = ""
+// Boolean => true ot false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// Object
+console.log(typeof age);  //typeof check value type
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // Object
