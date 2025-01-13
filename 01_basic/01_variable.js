4882310a8e0dd879bb277635ada56b03236a3ae6
gojosatoru112024 <gojosatoru112024@gmail.com>
2025-01-13 11:07:18 +0000

    variable done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 12345
+let accountName = "gojo@gmail.com"
+var accountNumber = "96587"
+accountCity = "Ahmedabad"
+let accountState;
+
+// accountId = 523652
+accountName = "test@gmail.com"
+accountNumber = "1234569870"
+accountCity = "Amdawad"
+
+// console.log(accountCity);
+
+console.table([accountId, accountName, accountNumber, accountCity, accountState])
